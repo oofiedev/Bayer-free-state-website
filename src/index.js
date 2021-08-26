985bc1da0f645fb5a8c10a31aef98fbce5cc96e8
oofiedev <tdkmike7406@gmail.com>
2021-08-26 23:27:14 +0700

    Fixed minor things

--- src/index.js
+++ src/index.js
@@ -12,26 +12,34 @@ const port = process.env.listenPort || 8080;
  */
 function serveFile(fName, callback) {
     fs.readFile(fName, 'utf8', (err, data) => {
-        if (err) { callback(null, err); return }
+        if (err) {
+            callback(null, err);
+            return;
+        }
         let req = data.match(/{{(.*?)}}/g);
-        if (!req) { callback(data); return }
+        if (!req) {
+            callback(data);
+            return;
+        }
         let count = req.length;
         for (let name of req) {
             name = name.substr(2, name.length - 4);
             fs.readFile('./root/comp/' + name, 'utf8', (err, dat) => {
-                if (err) { console.warn(err); }
+                if (err) {
+                    console.warn(err);
+                }
                 data = data.replace(/{{(.*?)}}/g, dat);
                 count--;
                 if (!count) callback(data);
             });
         }
-    })
+    });
 }
 
 app.get('/', (req, res) => {
     serveFile('./page/doc/constitution.html', (data) => {
         res.send(data);
-    })
+    });
 });
 
 app.post('/API/*', (req, res) => {});
@@ -41,11 +49,14 @@ app.use('/public', express.static('root/public'));
 app.get('/:name', (req, res) => {
     // pages (no .html)
     serveFile('./root/pages/' + req.params.name + '.html', (data, err) => {
-        if (err) { res.redirect('/404'); return }
+        if (err) {
+            res.redirect('/404');
+            return;
+        }
         res.send(data);
-    })
+    });
 });
 
 app.listen(port, () => {
     console.warn(`Listening at port ${port}.`);
-})
+});
